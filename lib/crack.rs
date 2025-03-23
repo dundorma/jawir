40cd894d847430d01933f90f87c8ab69def06ec0
dundorma <90293691+dundorma@users.noreply.github.com>
2025-03-23 15:30:34 +0700

    feat: add more algorithms

--- lib/crack.rs
+++ lib/crack.rs
@@ -43,6 +43,15 @@ fn get_validation(jwt: &str) -> Validation {
         "HS256" => Algorithm::HS256,
         "HS384" => Algorithm::HS384,
         "HS512" => Algorithm::HS512,
+        "RS256" => Algorithm::RS256,
+        "RS384" => Algorithm::RS384,
+        "RS512" => Algorithm::RS512,
+        "PS256" => Algorithm::PS256,
+        "PS384" => Algorithm::PS384,
+        "PS512" => Algorithm::PS512,
+        "ES256" => Algorithm::ES256,
+        "ES384" => Algorithm::ES384,
+        "EdDSA" => Algorithm::EdDSA,
         _ => panic!("Unsupported algorithm: {}", alg),
     };
 
